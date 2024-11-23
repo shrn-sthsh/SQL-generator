595451fc5e8ce395b12636562ed40f6270622761
Sharan Sathish <ssathish6@login-ice-1.pace.gatech.edu>
2024-11-23 14:41:06 -0500

    Hyperparameters in models as inference arugment

--- src/llm/gpt_neox_20b.py
+++ src/llm/gpt_neox_20b.py
@@ -4,5 +4,5 @@ class gpt_neox_llm(llm):
     def __init__(self, library_name: str = "EleutherAI/gpt-neox-20b"):
         super().__init__(library_name)
        
-    def infer(self, prompt: str, max_response_length: int = 100) -> str:
-        return super().infer(prompt, max_response_length) 
+    def infer(self, prompt: str, **hyperparameters) -> str:
+        return super().infer(prompt, hyperparameters=hyperparameters) 
--- src/llm/llama_11b.py
+++ src/llm/llama_11b.py
@@ -4,5 +4,5 @@ class llama_llm(llm):
     def __init__(self, library_name: str = "meta-llama/Llama-3.2-11B-Vision-Instruct"):
         super().__init__(library_name)
        
-    def infer(self, prompt: str, max_response_length: int = 100) -> str:
-        return super().infer(prompt, max_response_length) 
+    def infer(self, prompt: str, **hyperparameters) -> str:
+        return super().infer(prompt, hyperparameters=hyperparameters) 
